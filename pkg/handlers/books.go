c73cfa1a0b0140cd7e6f69061fadfc251c7824f1
D Ther Htun <dtherhtun.cw@gmail.com>
2025-06-10 23:03:46 +0700

    feat: DB operation with a repository pattern

--- pkg/handlers/books.go
+++ pkg/handlers/books.go
@@ -1,18 +1,10 @@
 package handlers
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"os"
-
-	"github.com/go-chi/chi/v5"
-
-	middleware "github.com/oapi-codegen/nethttp-middleware"
+	"context"
 
 	"BRSBackend/pkg/api"
+	"BRSBackend/pkg/models"
 	"BRSBackend/pkg/services"
 )
 
@@ -20,28 +12,49 @@ type BookHandler struct {
 	bookService services.BookService
 }
 
-func main() {
-	port := flag.String("port", "8080", "Port for test HTTP server")
-	flag.Parse()
-
-	swagger, err := api.GetSwagger()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
-		os.Exit(1)
+func NewBookHandler(bookService services.BookService) *BookHandler {
+	return &BookHandler{
+		bookService: bookService,
 	}
-
-	// Clear out the servers array in the swagger spec, that skips validating
-	// that server names match. We don't know how this thing will be run.
-	swagger.Servers = nil
-	petStore := api.NewPetStore()
-
-	r := chi.NewRouter()
-	r.Use(middleware.OapiRequestValidator(swagger))
-	api.HandlerFromMux(petStore, r)
-
-	s := &http.Server{
-		Handler: r,
-		Addr:    net.JoinHostPort("0.0.0.0", *port),
-	}
-	log.Fatal(s.ListenAndServe())
 }
+
+func (h *BookHandler) AddBook(ctx context.Context, request api.AddBookRequestObject) (api.AddBookResponseObject, error) {
+
+	book := models.Book{
+		Title:       request.Body.Title,
+		Description: request.Body.Description,
+		Count:       request.Body.Count,
+	}
+
+	if err, _ := h.bookService.CreateBook(ctx, &book); err != nil {
+		return api.AddBook500JSONResponse{}, nil
+	}
+
+	return api.AddBook201JSONResponse{}, nil
+}
+
+//func main() {
+//	port := flag.String("port", "8080", "Port for test HTTP server")
+//	flag.Parse()
+//
+//	swagger, err := api.GetSwagger()
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+//		os.Exit(1)
+//	}
+//
+//	// Clear out the servers array in the swagger spec, that skips validating
+//	// that server names match. We don't know how this thing will be run.
+//	swagger.Servers = nil
+//	petStore := api.NewPetStore()
+//
+//	r := chi.NewRouter()
+//	r.Use(middleware.OapiRequestValidator(swagger))
+//	api.HandlerFromMux(petStore, r)
+//
+//	s := &http.Server{
+//		Handler: r,
+//		Addr:    net.JoinHostPort("0.0.0.0", *port),
+//	}
+//	log.Fatal(s.ListenAndServe())
+//}
